0e61d8c2478c164107872e5d22492a2c76d3e90b
chenghx <chenghx@nfex.com>
2018-09-06 15:41:04 +0800

    refactor: change date utility libary: moment to date-fns

--- src/components/chart/MiniArea.vue
+++ src/components/chart/MiniArea.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import moment from 'moment'
+import {format} from 'date-fns'
 
 const data = []
 const beginDay = new Date().getTime()
@@ -18,7 +18,7 @@ const beginDay = new Date().getTime()
 const fakeY = [7, 5, 4, 2, 4, 7, 5, 6, 5, 9, 6, 3, 1, 5, 3, 6, 5]
 for (let i = 0; i < fakeY.length; i += 1) {
   data.push({
-    x: moment(new Date(beginDay + 1000 * 60 * 60 * 24 * i)).format('YYYY-MM-DD'),
+    x: format(new Date(beginDay + 1000 * 60 * 60 * 24 * i), 'YYYY-MM-DD'),
     y: fakeY[i]
   })
 }
