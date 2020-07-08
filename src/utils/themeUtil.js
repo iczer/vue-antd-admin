0e21e005cb790ea2014598fa522c196f6ff4e2a7
iczer <1126263215@qq.com>
2020-07-08 23:35:58 +0800

    fix: hover color problem in night mode; :bug: 修复：夜晚模式下 hover 颜色问题；

--- src/utils/themeUtil.js
+++ src/utils/themeUtil.js
@@ -28,6 +28,16 @@ module.exports = {
     const primary = palettes[5]
     return {
       'primary-color': primary,
+      'primary-1': palettes[0],
+      'primary-2': palettes[1],
+      'primary-3': palettes[2],
+      'primary-4': palettes[3],
+      'primary-5': palettes[4],
+      'primary-6': palettes[5],
+      'primary-7': palettes[6],
+      'primary-8': palettes[7],
+      'primary-9': palettes[8],
+      'primary-10': palettes[9],
       'info-color': primary,
       'alert-info-bg-color': palettes[0],
       'alert-info-border-color': palettes[3],
