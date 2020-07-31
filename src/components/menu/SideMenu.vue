4273ef86718d4ba7cb509bc10b5e499ae0bfcd36
iczer <1126263215@qq.com>
2020-07-31 22:24:18 +0800

    fix: the style problem caused by SideMenu.vue overflow; :bug: 修复：SideMenu.vue 组件溢出导致的样式问题；

--- src/components/menu/SideMenu.vue
+++ src/components/menu/SideMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout-sider :theme="sideTheme" :class="['side-menu', isMobile ? null : 'shadow']" width="256px" :collapsible="collapsible" v-model="collapsed" :trigger="null">
+  <a-layout-sider :theme="sideTheme" :class="['side-menu', 'beauty-scroll', isMobile ? null : 'shadow']" width="256px" :collapsible="collapsible" v-model="collapsed" :trigger="null">
     <div :class="['logo', theme]">
       <router-link to="/dashboard/workplace">
         <img src="@/assets/img/logo.png">
