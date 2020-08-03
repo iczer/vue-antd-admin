f82c33b0d3b9f81563ae1a2404385c660e8d3b31
iczer <1126263215@qq.com>
2020-08-03 21:35:50 +0800

    update configuration for docs deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Vue Antd Admin',
   description: 'Vue Antd Admin',
-  base: '/vue-antd-admin/',
+  base: '/vue-antd-admin-docs/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
