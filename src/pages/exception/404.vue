74dd33d936c0740c7b4d04283f614bb5f3cbe534
huangxianjun <xianjun.huang@ecarx.com.cn>
2021-03-19 12:11:20 +0800

    错误页面的返回首页改为/,提升通用性

--- src/pages/exception/404.vue
+++ src/pages/exception/404.vue
@@ -1,5 +1,5 @@
 <template>
-  <exception-page home-route="/dashboard/workplace" :style="`min-height: ${minHeight}`" type="404" />
+  <exception-page home-route="/" :style="`min-height: ${minHeight}`" type="404" />
 </template>
 
 <script>
